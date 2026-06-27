c118fc98b85ee4b0d040b95ed37b8164704fa287
RTCupid <19apr2006@vk.com>
2026-06-27 21:40:46 +0300

    update pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Artyom\'s Engineering Notes';
+export const SITE_DESCRIPTION = 'System programming, compilers, C++, AI, Python, backend, startup';
